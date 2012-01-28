7ce92928d9bf645d1c9d21eca1c16f21ddd28b6b
Peter Berkenbosch <peter@pero-ict.nl>
2012-01-28 14:59:42 +0100

    Initial spree (edge) install with minor layout fix.
    Signed-off-by: Peter Berkenbosch <peter@pero-ict.nl>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeDemoShop::Application.config.secret_token = '97ce64cc8b6b346272e89ce7583aa4935be472d9e7658043181ce8d6b344165d6abe1010c5ad9837a43840140cc114c611cb94a6b73994cfe58bf295b2152204'
